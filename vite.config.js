67d4f13ba847a7d4d8a1e74751683bba2396267b
chintu79 <shrikrishnachinta79@gmail.com>
2026-06-02 18:42:48 +0530

    fix: updated backend service link on vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,12 +11,12 @@ export default defineConfig({
       'a7c7-2401-4900-94f9-2ee4-b47c-d03f-c552-a6f1.ngrok-free.app'
     ],
     proxy: {
-      '/api': {
-        target: process.env.VITE_BACKEND_URL || 'http://localhost:3001',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
+    '/api': {
+      target: 'https://online-compiler-9fca.onrender.com',
+      changeOrigin: true,
+      rewrite: (path) => path.replace(/^\/api/, ''),
     },
+  },
   },
 })
 
