3ee43ad6996e5dc36d30983b606e3754b872df8a
Shreekrishna Chinta <113691576+chintu79@users.noreply.github.com>
2026-06-02 19:11:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0',
-    allowedHosts: [
-      'a7c7-2401-4900-94f9-2ee4-b47c-d03f-c552-a6f1.ngrok-free.app'
-    ],
+    allowedHosts:['webcompiler-yecv.onrender.com/'],
     proxy: {
     '/api': {
       target: 'https://online-compiler-9fca.onrender.com',
